b506a81c8a5d481ccb21e3d10ea133d56f437579
Alex <alexgofresh@gmail.com>
2024-02-13 17:22:15 +0100

    start backend

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <ul>
+        <li>menu</li>
+        <li>menu</li>
+        <li>menu</li>
+        <li>menu</li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
